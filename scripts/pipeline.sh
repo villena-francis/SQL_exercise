2b8ba374060641219be937ad39edf5ddac771e04
Francisco J. Villena <francis.villena@hotmail.es>
2024-02-19 23:23:22 +0100

    Add creation of database file

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -3,3 +3,12 @@ for url in $(cat data/url)
 do
     bash scripts/download.sh $url data
 done
+
+#Create ClinVar database file
+mkdir -p databases
+for file in data/variant_summary_*
+do
+    date=$(echo $file | grep -oP 'variant_summary_\K\d{4}-\d{2}' | sed 's/-/_/g')
+    database="databases/clinvar_${date}.db"
+    touch "$database"
+done 
