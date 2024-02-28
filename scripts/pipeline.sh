f19d46e0e4159692b0db3dba15085f270346c971
Francisco J. Villena <francis.villena@hotmail.es>
2024-02-29 00:15:32 +0100

    Add making of ClinVar database file

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,14 +4,12 @@ do
     bash scripts/download.sh $url data
 done
 
-#Create ClinVar database file
-mkdir -p databases
-for file in data/variant_summary_*
-do
-    date=$(echo $file | grep -oP 'variant_summary_\K\d{4}-\d{2}' | sed 's/-/_/g')
-    database="databases/clinvar_${date}.db"
-    touch "$database"
-done 
+#Create ClinVar database file 
+mkdir database
 
-#Include ClinVar variant data in the database
-python scripts/b_program.py $file
+for variant_summary in data/variant_summary_*
+do
+    date=$(echo $variant_summary | grep -oP 'variant_summary_\K\d{4}-\d{2}' | sed 's/-/_/g')
+    clinvar_db="database/clinvar_${date}.db"
+    touch "$clinvar_db"
+done
